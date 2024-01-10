d5b7238b484f0f2326732d9d9e199977f20eed42
rvs-teja <rvs.teja@jiva.ag>
2024-01-10 23:17:16 +0530

    first commit

---
+++ lib/wa_cloud_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "wa_cloud_api/version"
+
+module WaCloudApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/wa_cloud_api/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module WaCloudApi
+  VERSION = "0.1.0"
+end
